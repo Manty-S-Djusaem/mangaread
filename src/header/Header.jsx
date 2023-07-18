0eedab5fd927b151033e68873537d60b14189018
Manty-S-Djusaem <104922578+Manty-S-Djusaem@users.noreply.github.com>
2023-07-18 17:50:53 +0600

    done reg and signin

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,109 +5,103 @@ import Checkmark from '../checkmark/Checkmark'
 import Auth from '../ui/Authbtn'
 import Reg from '../ui/Regbtn'
 import Photo from '../assets/Photo.png'
+import { Typography } from '@mui/material';
 
 
 function Header(props) {
-    const [joinModalOpen, setJoinModalOpen] = useState(false);
-    const [regModalOpen, setRegModalOpen] = useState(false);
-    const [isChecked, setIsChecked] = useState(false);
+  const [joinModalOpen, setJoinModalOpen] = useState(false);
+  const [regModalOpen, setRegModalOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState('join'); // 'join' - по умолчанию
 
-    const handleClick = () => {
-        setIsChecked(!isChecked);
-    };
+  const handleOpenJoinModal = () => {
+    setJoinModalOpen(true);
+    setActiveTab('join');
+  };
 
-    const handleOpenJoinModal = () => {
-        setJoinModalOpen(true);
-    };
+  const handleCloseJoinModal = () => {
+    setJoinModalOpen(false);
+  };
 
-    const handleCloseJoinModal = () => {
-        setJoinModalOpen(false);
-    };
+  const handleOpenRegModal = () => {
+    setRegModalOpen(true);
+    setActiveTab('register');
+  };
 
-    const handleOpenRegModal = () => {
-        setRegModalOpen(true);
-    };
+  const handleCloseRegModal = () => {
+    setRegModalOpen(false);
+  };
 
-    const handleCloseRegModal = () => {
-        setRegModalOpen(false);
-    };
+  return (
+    <div className={module.container}>
+      <button className={module.openButtonJoin} onClick={handleOpenJoinModal}>
+        ВОЙТИ
+      </button>
 
-    return (
-        <div className={module.container}>
-            <button className={module.openButtonJoin} onClick={handleOpenJoinModal}>
-                ВОЙТИ
+      <button className={module.openButtonReg} onClick={handleOpenRegModal}>
+        РЕГИСТРАЦИЯ
+      </button>
+
+      {joinModalOpen && (
+        <div className={module.joinModalContainer}>
+          <div className={module.joinModal}>
+            <button className={module.closeButton} onClick={handleCloseJoinModal}>
+              <img src={close} className={module.clsbtn}></img>
             </button>
+            <span className={module.mouletext}>
+              <h1 className={module.hedtext + (activeTab === 'join' ? ' active' : '')}>Вход</h1>
+              <h1 className={module.hedtext1 + (activeTab === 'register' ? ' active' : '')}>Регистрация</h1>
+            </span>
+            <div
+              className={`underline ${activeTab === 'join' ? 'active' : ''}`}
+              style={{ width: '71px', borderBottom: '5px solid #AD02E0', marginLeft: '51px', marginTop: '-12px'}}
+            ></div>
+            <div className={module.modalContent}>
+              <input type='Username' placeholder='Username' />
+              <div className={module.modalContent2}>
+                <input type='Password' placeholder='Password' />
+              </div>
+              <Checkmark />
+            </div>
+            <Auth />
+          </div>
+        </div>
+      )}
 
-            <button className={module.openButtonReg} onClick={handleOpenRegModal}>
-                РЕГИСТРАЦИЯ
+      {regModalOpen && (
+        <div className={module.RegModalContainer}>
+          <div className={module.RegModal}>
+            <button className={module.closeButton} onClick={handleCloseRegModal}>
+              <img src={close} className={module.clsbtn}></img>
             </button>
-
-            {joinModalOpen && (
-                <div className={module.joinModalContainer}>
-                    <div className={module.joinModal}>
-                        <button className={module.closeButton} onClick={handleCloseJoinModal}>
-                            <img src={close} className={module.clsbtn}></img>
-                        </button>
-                        <span className={module.mouletext}>
-                            <h1 className={module.hedtext}>Вход</h1>
-                            <h1 className={module.hedtext1}>Регистрация</h1>
-                        </span>
-
-                        <div className={module.modalContent}>
-                            <input
-                                type='Username' placeholder='Username'
-                            />
-                            <div className={module.modalContent2}>
-                                <input
-                                    type='Password' placeholder='Password' />
-                            </div>
-                            <Checkmark />
-                        </div>
-                        <Auth />
-                    </div>
-                </div >
-
-
-            )
-            }
-
-            {regModalOpen && (
-                <div className={module.RegModalContainer}>
-                    <div className={module.RegModal}>
-                        <button className={module.closeButton} onClick={handleCloseRegModal}>
-                            <img src={close} className={module.clsbtn}></img>
-                        </button>
-                        <span className={module.mouletext}>
-                            <h1 className={module.hedtext}>Вход</h1>
-                            <h1 className={module.hedtext1}>Регистрация</h1>
-                        </span>
-
-                        <div className={module.modalContent}>
-                            <img src={Photo} className={module.photoReg} />
-                            <h1 className={module.photoText}>ДОБАВИТЬ ФОТО</h1>
-                            <div className={module.inputs}>
-                                <div className={module.modalContent3}>
-                                    <input
-                                        type='Username' placeholder='Username'
-                                    />
-                                </div>
-                                <div className={module.modalContent4}>
-                                    <input
-                                        type='Username' placeholder='Nickname' />
-                                </div>
-                                <div className={module.modalContent5}>
-                                    <input
-                                        type='Password' placeholder='Password' />
-                                </div>
-                            </div>
-                        </div>
-                        <Reg />
-                    </div>
+            <span className={module.mouletext}>
+              <h1 className={module.hedtext + (activeTab === 'join' ? ' active' : '')}>Вход</h1>
+              <h1 className={module.hedtext1 + (activeTab === 'register' ? ' active' : '')}>Регистрация</h1>
+            </span>
+            <div
+              className={`underline ${activeTab === 'register' ? 'active' : ''}`}
+              style={{ width: '164px', borderBottom: '5px solid #AD02E0', marginLeft: '142px', marginTop: '-12px'}}
+            ></div>
+            <div className={module.modalContent}>
+              <img src={Photo} className={module.photoReg} />
+              <h1 className={module.photoText}>ДОБАВИТЬ ФОТО</h1>
+              <div className={module.inputs}>
+                <div className={module.modalContent3}>
+                  <input type='Username' placeholder='Username' />
                 </div>
-            )
-            }
-        </div >
-    );
+                <div className={module.modalContent4}>
+                  <input type='Username' placeholder='Nickname' />
+                </div>
+                <div className={module.modalContent5}>
+                  <input type='Password' placeholder='Password' />
+                </div>
+              </div>
+            </div>
+            <Reg />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Header;
